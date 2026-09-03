7e905ba4a7e07b1e9dc8c9e013e60611714ff623
nguyenphuongvy <viedayyphuong@gmail.com>
2026-09-03 23:01:41 +0700

    Delete hamdieukien.py

--- hamdieukien.py
+++
@@ -1,7 +0,0 @@
-[hamdieukien.py](https://github.com/user-attachments/files/31798942/hamdieukien.py)
-lop = 10
-hocvienthamgia = 8
-if lop == hocvienthamgia :
-    print('Đủ')
-else :
-    print('Thiếu', lop - hocvienthamgia,'người')
